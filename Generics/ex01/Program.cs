f71bff10ab1ca4f413bf4d221c5f018dd173c67a
TheOlifve <hrant.hovhannisyan2413@gmail.com>
2026-06-28 17:15:11 +0400

    Generics Ex01

---
+++ Generics/ex01/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ex01;
+
+class GenericPair<T1, T2>
+{
+    public T1 First { get; }
+    public T2 Second { get; }
+    
+    public GenericPair(T1 first, T2 second)
+    {
+        First = first;
+        Second = second;
+    }
+
+    public GenericPair<T2, T1> SwapSides()
+    {
+        return new GenericPair<T2, T1>(Second, First);
+    }
+
+    public override string ToString()
+    {
+        return $"({First}, {Second})";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        GenericPair<int, string> x = new GenericPair<int, string>(7, "seven");
+        Console.WriteLine(x);
+        Console.WriteLine(x.SwapSides());
+    }
+}
