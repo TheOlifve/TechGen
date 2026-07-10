1873b0c496d522ef0b4bc2972ef14d7024c5f08c
TheOlifve <hrant.hovhannisyan2413@gmail.com>
2026-07-10 18:35:36 +0400

    ex03

--- FileInputOutput/ex03/PartialBinaryDownloader.cs
+++ FileInputOutput/ex03/PartialBinaryDownloader.cs
@@ -1,6 +1,19 @@
+using System.Reflection.Emit;
+
 namespace ex03;
 
-public class PartialBinaryDownloader
+public static class PartialBinaryDownloader
 {
+    static string _absolutePath = Path.GetFullPath(System.Environment.CurrentDirectory);
     
+    public static void DownloadChunk(string filename, byte[] chunk, int chunkOffset)
+    {
+        string filePath = _absolutePath + "/" + filename;
+        FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+        
+        fs.Seek(chunkOffset, SeekOrigin.Begin);
+        fs.Write(chunk, 0, chunk.Length);
+        
+        fs.Close();
+    }
 }
---
+++ FileInputOutput/ex03/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ex03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        byte[] blockA = new byte[]
+        {
+            34, 35, 36, 35, 38,
+            43, 42, 41, 40, 39
+        };
+
+        byte[] blockB = new byte[]
+        {
+            101, 102, 103, 104, 105,
+            106, 107, 108, 109, 110
+        };
+        
+        var fileName = "test.txt";
+        
+        PartialBinaryDownloader.DownloadChunk(fileName, blockA, 0);
+        PartialBinaryDownloader.DownloadChunk(fileName, blockB, 1024);
+    }
+}
