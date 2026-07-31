e958fe28f8d59a3015e6ca1ddc011a2957d56eb0
TheOlifve <hrant.hovhannisyan2413@gmail.com>
2026-07-31 10:27:19 +0400

    first commit

---
+++ MyApp/MyApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using ACA.PriceEngine;
+using WraperPriceEngine;
+
+namespace MyApp;
+
+class Program
+{
+    static void Fill(PriceInput input)
+    {
+        input.CouponAmount = 1;
+        BasketLine line = new BasketLine();
+        BasketLine line2 = new BasketLine();
+        BasketLine line3 = new BasketLine();
+        
+        line.Quantity = 10;
+        line2.Quantity = 20;
+        line3.Quantity = 30;
+
+        line.Sku = "AULD34KJ";
+        line2.Sku = "AU34KJ";
+        line3.Sku = "AULKJ";
+
+        line.UnitPrice = 16;
+        line2.UnitPrice = 12;
+        line3.UnitPrice = 13;
+        
+        input.Lines.Add(line);
+        input.Lines.Add(line2);
+        input.Lines.Add(line3);
+        
+        input.LoyaltyTier = 29;
+        input.VatRate = 10;
+    }
+    
+    static void Main(string[] args)
+    {
+        WrapPriceEngine wrapedEngine = new WrapPriceEngine();
+        PriceEngine priceEngine = new PriceEngine();
+        PriceInput input = new PriceInput();
+        Fill(input);
+        
+        Console.WriteLine($"Price Engine - {priceEngine.CalculatePayable(input)}");
+        Console.WriteLine($"Wraped Engine - {wrapedEngine.CalculatePayable(input)}");
+    }
+}
---
+++ MyApp/WraperPriceEngine/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using ACA.PriceEngine;
+
+namespace WraperPriceEngine;
+
+public class WrapPriceEngine
+{
+    PriceEngine _engine = new PriceEngine();
+    
+    public decimal CalculatePayable(PriceInput input)
+    {
+        Type type =  typeof(PriceEngine);
+
+        decimal amount = (decimal)type.GetMethod("ComputeSubtotal", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(_engine, new object[] { input.Lines});
+        
+        amount = (decimal)type.GetMethod("ApplyVolumeDiscount", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(_engine, new object[] { amount,
+            type.GetMethod("CountUnits",  BindingFlags.NonPublic | BindingFlags.Instance).Invoke(_engine, new object[] { input.Lines }) });
+        
+        amount = (decimal)type.GetMethod("ApplyLoyaltyDiscount", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(_engine, new object[] { amount, input.LoyaltyTier });
+        
+        amount = (decimal)type.GetMethod("ApplyCoupon", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(_engine, new object[] { amount, input.CouponAmount });
+        
+        amount = (decimal)type.GetMethod("ApplyVat", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(_engine, new object[] { amount, input.VatRate });
+        
+        return (decimal)type.GetMethod("RoundMoney", BindingFlags.NonPublic | BindingFlags.Static).Invoke(null, new object[] { amount });
+    }
+    
+}
